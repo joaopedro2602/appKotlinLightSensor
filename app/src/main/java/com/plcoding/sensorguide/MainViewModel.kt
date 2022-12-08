3924de1fda8c75d30506d3e3a3502b520e78e4fb
joaopedro2602 <joao.cabral14@etec.sp.gov.br>
2022-12-07 22:06:35 -0300

    projeto em kotlin utilizando o sensor de luz do celular

---
+++ app/src/main/java/com/plcoding/sensorguide/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.plcoding.sensorguide
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val lightSensor: MeasurableSensor
+): ViewModel() {
+
+    var isDark by mutableStateOf(false)
+    val luz = 0
+    init {
+        lightSensor.startListening()
+        lightSensor.setOnSensorValuesChangedListener { values ->
+            val lux = values[0]
+            isDark = lux < 13f
+
+        }
+    }
+}
